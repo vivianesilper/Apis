a878590470ba715986d57ed5b6faaa16c7025180
Viviane Pereira <vivianesilper@gmail.com>
2025-01-10 14:38:41 -0300

    Create_app

--- app.py
+++ app.py
@@ -4,17 +4,12 @@ from flask import Flask
 from flask import jsonify
 from flask_restful import Api, Resource
 
-from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
-from purchase_orders_items.resources import PurchaseOrdersItems 
 
 
-app = Flask(__name__)
-api = Api(app)
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
 
-
-api.add_resource(PurchaseOrders, '/purchase_orders')  
-api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
-api.add_resource(PurchaseOrdersItems, '/purchase_orders/<int:id>/items')
-
-if __name__ == '__main__':
- app.run(port=5000)
+    app.run(port=5000)
+    
+    return app
