365854cf7d60c0727994bfc254e3a97e64bd7b36
Viviane Pereira <vivianesilper@gmail.com>
2025-01-13 14:46:38 -0300

    create conftest.py

--- app.py
+++ app.py
@@ -10,6 +10,4 @@ def create_app():
     app = Flask(__name__)
     api = Api(app)
 
-    app.run(port=5000)
-    
     return app
---
+++ teste/purchase_orders/test_resources.py
@@ -0,0 +1,5 @@
+def test_get_purchase_orders(test_client):
+    response = test_client.get('purchase_orders')
+    
+    assert response.status_code == 200
+    
