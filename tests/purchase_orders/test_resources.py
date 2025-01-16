8cf328c5f0f4b197e144f554471522f3e0925991
Viviane Pereira <vivianesilper@gmail.com>
2025-01-16 17:15:30 -0300

    criado o documento test_reources.py na test/purchase_orders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -6,4 +6,5 @@ def test_get_purchase_orders(test_client):
     assert response.json[0]['description'] == 'Purchase Order id 1'
     assert len(response.json[0]['items']) == 1
     assert response.json[0]['items']['id'] == 1
+
     
