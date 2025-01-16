4f7656ec3ab9bd6920c6ed78c1aa71d4d4679b11
Viviane Pereira <vivianesilper@gmail.com>
2025-01-16 15:37:45 -0300

    Refatorando para criar novo recurso purchase_orders

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 # This Python code snippet is setting up a Flask application with RESTful API endpoints for managing
 # purchase orders and their items. Here's a breakdown of what each part does:
 from flask import Flask
-from flask import jsonify
 from flask_restful import Api
 
 from purchase_orders.resources import PurchaseOrders
 
-
-
+app = Flask(__name__)
+api = Api(app)
 def create_app():
     app = Flask(__name__)
     api = Api(app)
     
-    api.add_resource(PurchaseOrders, '/purchase_orders')
 
     
-    return app
+api.add_resource(PurchaseOrders, '/purchase_orders')
+        
+    
+app.run(port=5000)
--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -4,8 +4,8 @@ from flask_restful import Resource
 purchase_orders = [
     {
         'id': 1,
-        'description': 'Purchase Order id 1',
-        'items':[
+        'description': 'Pedido de compra 1',
+        'items': [
             {
                 'id': 1,
                 'description': 'Item do pedido 1',
@@ -14,8 +14,7 @@ purchase_orders = [
         ]
     }
 ]
+
 class PurchaseOrders(Resource):
-    
     def get(self):
         return jsonify(purchase_orders)
-    
