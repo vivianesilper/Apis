588a7c8d4b059e9fed2e74804abe75a399e59a20
Viviane Pereira <vivianesilper@gmail.com>
2025-01-16 17:44:44 -0300

    test_post_purchase_orders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -14,7 +14,7 @@ def test_post_purchase_orders(test_client):
     
     response = test_client.post(
         '/purchase_orders',
-        data=json.dumps(obj)
+        data=json.dumps(obj),
         content_type='application/json'
     )
     
